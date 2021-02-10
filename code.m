1f1d03bf67aae78c2c1b76b959b4b2376bb68490
lvarshini3096 <78391474+lvarshini3096@users.noreply.github.com>
2021-02-10 00:12:46 -0500

    Update and rename code to code.m

--- code.m
+++ code.m
@@ -1,28 +1,35 @@
 close all
 clear all
+
 % Number samples in each frame
 N = 256;
+
 % Input files from primary mic and secondary mic
 % For background music change the input files here to mic1_music.wav and
 % mic2_music.wav
 prim = 'mic1.wav';
 seco = 'mic2.wav';
 sample = 'cleanspeech.wav';
+
 % Reading input files using audioread function
 [mic1,fs1] = audioread(prim);
 [mic2,fs2] = audioread(seco);
 [clean,fs3 ] = audioread(sample);
+
 % Finding length of each input signals, for inputs with music length is set
 as 60,000 samples
 l1 = length(mic1(1:60000));
 l2 = length(mic2(1:60000));
+
 % Finding the minimum value between two inputs
-12
 M = min(l1,l2);
+
 % Calculations of number of frames
 Nframes = fix(M/N);
+
 % Setting the value of mu
 mu = 0.002973;
+
 % Initialization of variables
 d = mic1;
 x = mic2;
@@ -39,34 +46,34 @@ SNRa = 0;
 
 % Calculation of E, B for each frame
 for i = 1:Nframes
- n = (1:N)+(N*(i-1));
+    n = (1:N)+(N*(i-1));
 
- % Peforming FFT operation
- X = fft(x(n));
- D = fft(d(n));
+    % Peforming FFT operation
+    X = fft(x(n));
+    D = fft(d(n));
 
- % Converting elements X(w) into a diagonal matix
- Xdiag = diag(X);
+    % Converting elements X(w) into a diagonal matix
+    Xdiag = diag(X);
 
- % Storing the values of B in a matrix
- Bs(:,i) = B;
+    % Storing the values of B in a matrix
+    Bs(:,i) = B;
 
- % Calculating the value of E for a frame
- E(:,i) = D - (Xdiag*B);
- B = B + (2*mu*Xdiag'*E(:,i));
+    % Calculating the value of E for a frame
+    E(:,i) = D - (Xdiag*B);
+    B = B + (2*mu*Xdiag'*E(:,i));
 
- % Inverse Fourier Transform and storing it in matrix e with dimensions
- % Nframes x N
- e(i,:) = ifft(E(:,i));
+    % Inverse Fourier Transform and storing it in matrix e with dimensions
+    % Nframes x N
+    e(i,:) = ifft(E(:,i));
 
- % Taking the magnitude squares for s(n), mic1(n), e(n)
- E1(i) = ((clean(n)')*clean(n))/N;
- E2(i) = ((mic1(n)')*mic1(n))/N;
- E3(i) = ((e(i,:))*e(i,:)')/N;
+    % Taking the magnitude squares for s(n), mic1(n), e(n)
+    E1(i) = ((clean(n)')*clean(n))/N;
+    E2(i) = ((mic1(n)')*mic1(n))/N;
+    E3(i) = ((e(i,:))*e(i,:)')/N;
 
- % Calculation of SNR before and SNR after for each frame
- SNRbefore(i) = 10*log10((E1(i))/(E1(i)+E2(i)-(2*sqrt(E1(i)*E2(i)))));
- SNRafter(i) = 10*log10((E1(i))/(E1(i)+E3(i)-(2*sqrt(E1(i)*E3(i)))));
+    % Calculation of SNR before and SNR after for each frame
+    SNRbefore(i) = 10*log10((E1(i))/(E1(i)+E2(i)-(2*sqrt(E1(i)*E2(i)))));
+    SNRafter(i) = 10*log10((E1(i))/(E1(i)+E3(i)-(2*sqrt(E1(i)*E3(i)))));
 end
 
  % Calculation of Average SNR before ANC
@@ -75,7 +82,6 @@ end
  % Calculation of Average SNR after ANC
  SNRa = mean(SNRafter);
 
-13
  % Calculation of SNR improvement
  SNRimp = SNRa - SNRb;
 
